26c21b978a328a904268959aea2171ca944ac787
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2023-08-03 11:25:00 +0900

    ✨ 사람 얼굴 그리기

--- app.js
+++ app.js
@@ -4,11 +4,15 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.fillRect(200, 200, 50, 200);
-ctx.fillRect(400, 200, 50, 200);
-ctx.fillRect(300, 300, 50, 100);
-ctx.fillRect(200, 200, 200, 20);
-ctx.moveTo(200, 200);
-ctx.lineTo(325, 100);
-ctx.lineTo(450, 200);
+ctx.fillRect(200, 200, 20, 100);
+ctx.fillRect(400, 200, 20, 100);
+ctx.fillRect(270, 200, 80, 200);
+
+ctx.arc(310, 100, 40, 0, 2 * Math.PI);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = "white";
+ctx.arc(295, 90, 5, Math.PI, 2 * Math.PI);
+ctx.arc(325, 90, 5, Math.PI, 2 * Math.PI);
 ctx.fill();
