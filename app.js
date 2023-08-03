2f6f538cf4360019638e9ceec9944e74b34a41f3
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2023-08-03 14:47:42 +0900

    ✨ 마우스 움직임에 따라 선그리기
    마우스를 눌렀을 때 선이 따라오고 마우스를 떼면 선이 끊어짐

--- app.js
+++ app.js
@@ -3,29 +3,28 @@ const ctx = canvas.getContext("2d");
 
 canvas.width = 800;
 canvas.height = 800;
-
 ctx.lineWidth = 2;
 
-const colors = [
-  "#55efc4",
-  "#00cec9",
-  "#ff7675",
-  "#e84393",
-  "#81ecec",
-  "#0984e3",
-  "#fd79a8",
-  "#74b9ff",
-  "#ffeaa7",
-  "#fab1a0",
-];
+let isPainting = false;
 
 function onMove(event) {
-  ctx.beginPath();
-  ctx.moveTo(0, 0);
-  const color = colors[Math.floor(Math.random() * colors.length)];
-  ctx.strokeStyle = color;
-  ctx.lineTo(event.offsetX, event.offsetY);
-  ctx.stroke();
+  if (isPainting) {
+    ctx.lineTo(event.offsetX, event.offsetY);
+    ctx.stroke();
+    return;
+  }
+  ctx.moveTo(event.offsetX, event.offsetY);
+}
+
+function startPainting() {
+  isPainting = true;
+}
+
+function cancelPainting() {
+  isPainting = false;
 }
 
 canvas.addEventListener("mousemove", onMove);
+canvas.addEventListener("mousedown", startPainting);
+canvas.addEventListener("mouseup", cancelPainting);
+canvas.addEventListener("mouseleave", cancelPainting);
