a83fd9e7330e10259f4eda9cac87011d00288959
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2023-08-03 11:00:57 +0900

    ✨ 집 그리기

--- app.js
+++ app.js
@@ -4,15 +4,11 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-// 사각형을 그리는 두 가지 방법
-//  1. fillRect(x, y, w, h)
-ctx.fillRect(50, 50, 100, 100);
-
-//  2. rect(), fill() -> rect()을 쓸 경우 fill() 메소드를 쓰지 않으면 채워지지 않음
-ctx.rect(150, 150, 100, 100);
-ctx.fill();
-
-ctx.beginPath();
-ctx.rect(250, 250, 100, 100);
-ctx.fillStyle = "red";
+ctx.fillRect(200, 200, 50, 200);
+ctx.fillRect(400, 200, 50, 200);
+ctx.fillRect(300, 300, 50, 100);
+ctx.fillRect(200, 200, 200, 20);
+ctx.moveTo(200, 200);
+ctx.lineTo(325, 100);
+ctx.lineTo(450, 200);
 ctx.fill();
