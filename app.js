3a99c2f0d89265d1ec3816555dd2382223233f28
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2023-08-03 10:41:09 +0900

    ✨ 사각형 나눠서 색칠하기

--- app.js
+++ app.js
@@ -9,5 +9,10 @@ canvas.height = 800;
 ctx.fillRect(50, 50, 100, 100);
 
 //  2. rect(), fill() -> rect()을 쓸 경우 fill() 메소드를 쓰지 않으면 채워지지 않음
+ctx.rect(150, 150, 100, 100);
+ctx.fill();
+
+ctx.beginPath();
 ctx.rect(250, 250, 100, 100);
+ctx.fillStyle = "red";
 ctx.fill();
